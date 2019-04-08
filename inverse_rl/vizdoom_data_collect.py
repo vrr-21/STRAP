2c91515e8ab22ad630569afbb47fc07c16eda471
sreekar <sreekarkamireddy@yahoo.com>
2019-04-08 15:15:51 -0700

    'Changed the vizdoom_datacollect and irl'

--- inverse_rl/vizdoom_data_collect.py
+++ inverse_rl/vizdoom_data_collect.py
@@ -2,6 +2,7 @@ from sandbox.rocky.tf.algos.trpo import TRPO
 from sandbox.rocky.tf.envs.base import TfEnv
 from sandbox.rocky.tf.policies.categorical_conv_policy import CategoricalConvPolicy
 from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.baselines.zero_baseline import ZeroBaseline
 from rllab.envs.gym_env import GymEnv
 from rllab.envs.normalized_env import normalize
 from inverse_rl.utils.log_utils import rllab_logdir
@@ -13,11 +14,11 @@ def main():
     env = TfEnv((GymEnv(env_name+'-v0', record_video=False, record_log=False)))
     # policy = GaussianMLPPolicy(name='policy', env_spec=env.spec, hidden_sizes=(32, 32))
     policy = CategoricalConvPolicy(
-        name='policy', 
+        name='policy',
         env_spec=env.spec,
-        conv_filters=[32,64,64], 
-        conv_filter_sizes=[3]*3, 
-        conv_strides=[1]*3, 
+        conv_filters=[32,64,64],
+        conv_filter_sizes=[3]*3,
+        conv_strides=[1]*3,
         conv_pads=['SAME']*3
     )
 
@@ -25,11 +26,11 @@ def main():
         env=env,
         policy=policy,
         n_itr=N_ITERATIONS,
-        batch_size=100,
-        max_path_length=20,
+        batch_size=200,
+        max_path_length=200,
         discount=0.99,
         store_paths=True,
-        baseline=LinearFeatureBaseline(env_spec=env.spec)
+        baseline=ZeroBaseline(env_spec=env.spec)
     )
 
     with rllab_logdir(algo=algo, dirname='data/'+env_name):
--- inverse_rl/vizdoom_irl.py
+++ inverse_rl/vizdoom_irl.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from sandbox.rocky.tf.policies.categorical_conv_policy import CategoricalConvPolicy
 from sandbox.rocky.tf.envs.base import TfEnv
 from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.baselines.zero_baseline import ZeroBaseline
 from rllab.envs.gym_env import GymEnv
 from inverse_rl.models.architectures import conv_net
 
@@ -17,20 +18,20 @@ import sys
 def main():
     env_name = sys.argv[1]
     env = TfEnv(GymEnv(env_name+'-v0', record_video=False, record_log=False))
-    
-    experts = load_latest_experts('data/'+env_name, n=3)
+
+    experts = load_latest_experts('data/'+env_name, n=5)
 
     irl_model = AIRLStateAction(
-        env_spec=env.spec, 
+        env_spec=env.spec,
         expert_trajs=experts
     )
 
     policy = CategoricalConvPolicy(
-        name='policy', 
+        name='policy',
         env_spec=env.spec,
-        conv_filters=[32, 64, 64], 
-        conv_filter_sizes=[3] * 3, 
-        conv_strides=[1] * 3, 
+        conv_filters=[32, 64, 64],
+        conv_filter_sizes=[3] * 3,
+        conv_strides=[1] * 3,
         conv_pads=['SAME'] * 3
     )
 
@@ -39,22 +40,22 @@ def main():
         policy=policy,
         irl_model=irl_model,
         n_itr=N_ITERATIONS,
-        batch_size=100,
-        max_path_length=10,
+        batch_size=200,
+        max_path_length=200,
         discount=0.99,
         store_paths=True,
         discrim_train_itrs=1,
         irl_model_wt=1.0,
         entropy_weight=0.1, # this should be 1.0 but 0.1 seems to work better
         zero_environment_reward=True,
-        baseline=LinearFeatureBaseline(env_spec=env.spec)
+        baseline=ZeroBaseline(env_spec=env.spec)
     )
 
     saver = tf.train.Saver()
     with rllab_logdir(algo=algo, dirname='data/'+env_name+'_gcl'):
         with tf.Session() as sess:
             algo.train()
-            saver.save(sess, '/home/tejas/Workspace/STRAP/inverse_rl/irl_models/model_'+env_name)
+            saver.save(sess, './model_'+env_name)
 
 if __name__ == "__main__":
     main()
